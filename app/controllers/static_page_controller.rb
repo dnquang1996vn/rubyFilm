94de908cf058fe39e3939d419eef36713f1266b1
Buithienbao <thienbao_bui@yahoo.com>
2018-04-27 23:12:28 +0700

    add href to menu

--- app/controllers/static_page_controller.rb
+++ app/controllers/static_page_controller.rb
@@ -1,4 +1,31 @@
 class StaticPageController < ApplicationController
   def index
   end
+  def discover_movies
+	render "discover_movies"
+  end
+  def popular
+	render "popular"
+  end
+  def top_rated
+	render "top_rated"
+  end
+  def now_playing
+	render "now_playing"
+  end
+  def upcoming
+	render "upcoming"
+  end
+  def people
+	render "people"
+  end
+  def login
+	render "login"
+  end
+  def register
+  	render "register"
+  end
+  def film_info
+ 	render "film_info"
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,14 @@
 Rails.application.routes.draw do
   get 'static_page/index'
   get 'welcome/index'
-
+  get 'static_page/discover', to: 'static_page#discover_movies'
+  get 'static_page/now_playing', to: 'static_page#now_playing'
+  get 'static_page/popular', to: 'static_page#popular'
+  get 'static_page/top_rated', to: 'static_page#top_rated'
+  get 'static_page/upcoming', to: 'static_page#upcoming'
+  get 'static_page/login', to: 'static_page#login'
+  get 'static_page/register', to: 'static_page#register'
+  get 'static_page/people', to: 'static_page#people'
+  get 'static_page/film_info', to: 'static_page#film_info'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
