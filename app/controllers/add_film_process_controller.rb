2f93e5a5481cf38b3f1cf996efd7f601db343c73
tadt <tadt@ows.com.vn>
2018-05-12 23:29:16 +0700

    add film process

---
+++ app/controllers/add_film_process_controller.rb
@@ -0,0 +1,50 @@
+class AddFilmProcessController < ApplicationController
+
+  def index
+
+    @name = params[:name]
+    @released_date = params[:released_date]
+    @genre = params[:genre]
+    @description = params[:description]
+    @img = params[:img]
+    @trailer = params[:trailer]
+    @length = params[:length]
+    @rate = params[:rate]
+    @mat = params[:mat]
+    @directors = params[:directors]
+    @casts = params[:casts]
+    @roles = params[:roles]
+
+    puts @name
+
+    film = Film.new do |u|
+      u.name = @name
+      u.released_date = @released_date
+      u.genre = @genre
+      u.description = @description
+      u.img = @img
+      u.trailer = @trailer
+      u.length = @length
+      u.rate = @rate
+      u.mat = @mat
+      u.directors = @directors
+      u.casts = @casts
+      u.roles = @roles
+    end
+
+    film.save
+  end
+
+end
+name: name,
+    released_date: released_date,
+    genre: genre,
+    description: description,
+    img: img,
+    trailer: trailer,
+    length: length,
+    rate: rate,
+    mat: mat,
+    directors: directors,
+    casts: casts,
+    roles: roles
---
+++ app/helpers/add_film_process_helper.rb
@@ -0,0 +1,2 @@
+module AddFilmProcessHelper
+end
