01704273b4d6124bbfe2ea1768668f73695604ae
Buithienbao <thienbao_bui@yahoo.com>
2018-05-12 16:06:56 +0700

    complete top rated

--- app/controllers/top_rated_controller.rb
+++ app/controllers/top_rated_controller.rb
@@ -1,6 +1,6 @@
 class TopRatedController < ApplicationController
   def index
-  	@films = Film.where("released_date < ?",Date.today).order('released_date DESC').page(params[:page]).per(10)
-  	render "now_playing/index"
+  	@films = Film.order('avg_rate DESC').page(params[:page]).per(10)
+  	render "top_rated/index"
   end
 end
